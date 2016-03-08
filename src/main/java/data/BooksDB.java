e5ec4847a6d612c769652fb30927261a656c2850
p4ndepravity <trent.bullard@gmail.com>
2016-03-08 15:54:14 -0600

    create book model; configure bookdb for properties file;

--- src/main/java/data/BooksDB.java
+++ src/main/java/data/BooksDB.java
@@ -1,10 +1,12 @@
 package data;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class BooksDB {
 
@@ -16,40 +18,63 @@ public class BooksDB {
 	public BooksDB() throws Exception {
 
 		try {
-			// Get a connection to database
-			dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "library", "library");
+			// Load the properties file
+			Properties props = new Properties();
+			props.load(new FileInputStream("db.properties"));
 
+			// Read the props
+			String theUser = props.getProperty("user");
+			String thePassword = props.getProperty("password");
+			String theDburl = props.getProperty("dburl");
+			
+			dbConnection = DriverManager.getConnection(theDburl, 
+													   theUser, 
+													   thePassword);
+	
 			// Create the statement
 			dbStatement = dbConnection.createStatement();
-
-			rs = dbStatement.executeQuery("select * from Books order by author_last_name desc");
+	
+			rs = dbStatement.executeQuery("select * from Books "
+										+ "order by author_last_name desc");
 			while (rs.next()) {
 				count++;
 			}
-
+	
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
 
 	}
 
-	public void add(String id, String title, String authLName, String authFName, int rating) throws SQLException {
-		String s = "insert into library.books (book_id,title,author_last_name,author_first_name,rating) " + "values ('"
-				+ id + "','" + title + "','" + authLName + "','" + authFName + "'," + rating + ")";
-		int rowsAffected = dbStatement.executeUpdate(s);
+	public void add(String id, 
+					String title, 
+					String authLName, 
+					String authFName, 
+					int rating) throws SQLException {
+		String s = "insert into library.books "
+				+ "(book_id,title,author_last_name,author_first_name,rating) "
+				+ "values ('" + id + "',"
+						+ "'" + title + "',"
+						+ "'" + authLName + "',"
+						+ "'" + authFName + "',"
+							  + rating + ")";
+		dbStatement.executeUpdate(s);
 		count++;
-		rs = dbStatement.executeQuery("select * from Books order by author_last_name desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "order by author_last_name desc");
 	}
 
 	public void displayAll() throws SQLException {
 		String title;
 		String author;
 
-		rs = dbStatement.executeQuery("select * from Books order by author_last_name desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "order by author_last_name desc");
 
 		while (rs.next()) {
 			title = rs.getString("title");
-			author = rs.getString("author_first_name") + " " + rs.getString("author_last_name");
+			author = rs.getString("author_first_name") + " " 
+				   + rs.getString("author_last_name");
 			System.out.println(title + " by " + author);
 		}
 	}
@@ -61,75 +86,93 @@ public class BooksDB {
 		while (rs.next()) {
 			System.out.println("id: " + rs.getString("book_id"));
 			System.out.println("title: " + rs.getString("title"));
-			String author = rs.getString("author_first_name") + " " + rs.getString("author_last_name");
+			String author = rs.getString("author_first_name") + " " 
+						  + rs.getString("author_last_name");
 			System.out.println("author: " + author);
 			System.out.println("rating: " + rs.getString("rating") + "\n\n");
 		}
 
-		rs = dbStatement.executeQuery("select * from Books order by author_last_name desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "order by author_last_name desc");
 	}
 
 	public void returnBooksByAuthLastName(String s) throws SQLException {
-		rs = dbStatement.executeQuery("select * from Books where author_last_name like '%" + s + "%' order by author_last_name desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "where author_last_name like '%" + s + "%' "
+									+ "order by author_last_name desc");
 		String title;
 		String author;
 		String id;
 		while (rs.next()) {
 			id = rs.getString("book_id");
 			title = rs.getString("title");
-			author = rs.getString("author_first_name") + " " + rs.getString("author_last_name");
+			author = rs.getString("author_first_name") + " " 
+				   + rs.getString("author_last_name");
 			System.out.println(id + " " + title + " by " + author + "\n\n");
 		}
 	}
 
 	public void returnBooksByAuthFirstName(String s) throws SQLException {
-		rs = dbStatement.executeQuery("select * from Books where author_first_name like '%" + s + "%' order by author_first_name desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "where author_first_name like '%" + s + "%' "
+									+ "order by author_first_name desc");
 		String title;
 		String author;
 		String id;
 		while (rs.next()) {
 			id = rs.getString("book_id");
 			title = rs.getString("title");
-			author = rs.getString("author_first_name") + " " + rs.getString("author_last_name");
+			author = rs.getString("author_first_name") + " " 
+				   + rs.getString("author_last_name");
 			System.out.println(id + " " + title + " by " + author + "\n\n");
 		}
 	}
 
 	public void returnBooksByTitle(String s) throws SQLException {
-		rs = dbStatement.executeQuery("select * from Books where title like '%" + s + "%' order by title desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "where title like '%" + s + "%' "
+									+ "order by title desc");
 		String title;
 		String author;
 		String id;
 		while (rs.next()) {
 			id = rs.getString("book_id");
 			title = rs.getString("title");
-			author = rs.getString("author_first_name") + " " + rs.getString("author_last_name");
+			author = rs.getString("author_first_name") + " " 
+				   + rs.getString("author_last_name");
 			System.out.println(id + " " + title + " by " + author + "\n\n");
 		}
 	}
 	
 	public void returnBooksByRating(int choice) throws SQLException {
-		rs = dbStatement.executeQuery("select * from Books where rating like " + choice + " order by title desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "where rating like " + choice + " "
+									+ "order by title desc");
 		String title;
 		String author;
 		String id;
 		while (rs.next()) {
 			id = rs.getString("book_id");
 			title = rs.getString("title");
-			author = rs.getString("author_first_name") + " " + rs.getString("author_last_name");
+			author = rs.getString("author_first_name") + " " 
+				   + rs.getString("author_last_name");
 			System.out.println(id + " " + title + " by " + author + "\n\n");
 		}
 	}
 	
-	public void change(String id, String column, String newValue) throws SQLException {
-		String query = "update Books set " + column + "='" + newValue + "' where book_id='" + id + "'";
-		int affectedRows = dbStatement.executeUpdate(query);
+	public void change(String id, 
+					   String column, 
+					   String newValue) throws SQLException {
+		String query = "update Books set " + column + "='" + newValue + "' "
+					 + "where book_id='" + id + "'";
+		dbStatement.executeUpdate(query);
 
-		rs = dbStatement.executeQuery("select * from Books order by author_last_name desc");
+		rs = dbStatement.executeQuery("select * from Books "
+									+ "order by author_last_name desc");
 	}
 	
 	public void delete(String s) throws SQLException {
-		int rowsAffected = dbStatement.executeUpdate("delete from Books where book_id='" + s + "'");
+		dbStatement.executeUpdate("delete from Books where book_id='" + s + "'");
 	}
 
 	public void close() throws SQLException {
---
+++ src/main/java/models/Book.java
@@ -0,0 +1,78 @@
+package models;
+
+public class Book {
+	private String id;
+	private String title;
+	private String authorLastName;
+	private String authorFirstName;
+	private int rating;
+	
+	public Book() {
+		setid("none");
+		setTitle("title");
+		setAuthorLastName("lastname");
+		setAuthorFirstName("firstname");
+		setRating(5);
+	}
+	
+	public Book(String id, 
+				String title, 
+				String authorLastName, 
+				String authorFirstName, 
+				int rating) {
+		this.id = id;
+		this.title = title;
+		this.authorLastName = authorLastName;
+		this.authorFirstName = authorFirstName;
+		this.rating = rating;
+	}
+
+	public String getid() {
+		return id;
+	}
+
+	public void setid(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthorLastName() {
+		return authorLastName;
+	}
+
+	public void setAuthorLastName(String authorLastName) {
+		this.authorLastName = authorLastName;
+	}
+
+	public String getAuthorFirstName() {
+		return authorFirstName;
+	}
+
+	public void setAuthorFirstName(String authorFirstName) {
+		this.authorFirstName = authorFirstName;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public boolean equals(Book other) {
+		return this.id == other.id ? true : false;
+	}
+
+	@Override
+	public String toString() {
+		return this.title + " by " + this.authorFirstName + " " + this.authorLastName;
+	}
+}
